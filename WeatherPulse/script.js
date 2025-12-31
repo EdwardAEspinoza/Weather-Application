4ee3feebcc04f83bc3262a17db7d95c9ab76ef8d
Edward Espinoza <edwespino@gmail.com>
2025-12-31 00:50:58 +0000

    Add 5-day forecast for WeatherPulse

--- WeatherPulse/script.js
+++ WeatherPulse/script.js
@@ -1,5 +1,6 @@
 const apiKey = "28d49ae1b3bf92f79d47d6238d907b37";
 const weatherUrl = "https://api.openweathermap.org/data/2.5/weather?units=imperial&q=";
+const forecastUrl = "https://api.openweathermap.org/data/2.5/forecast?units=imperial&q=";
 
 const searchBox = document.querySelector(".search input");
 const searchButton = document.querySelector(".search button");
@@ -11,40 +12,90 @@ async function checkingWeather(city){
     if(response.status == 404){
         document.querySelector(".error").style.display = "block";
         document.querySelector(".weather").style.display = "none";
-    }else{
-        var data = await response.json();
+        document.querySelector(".forecast").style.display = "none";
+    } else {
+        const data = await response.json();
        
-document.querySelector(".city").innerHTML = data.name;
-document.querySelector(".temp").innerHTML = Math.round(data.main.temp) + "°F";
-document.querySelector(".humidity").innerHTML = data.main.humidity + "%";
-document.querySelector(".wind").innerHTML = data.wind.speed + "mph"; 
+        document.querySelector(".city").innerHTML = data.name;
+        document.querySelector(".temp").innerHTML = Math.round(data.main.temp) + "°F";
+        document.querySelector(".humidity").innerHTML = data.main.humidity + "%";
+        document.querySelector(".wind").innerHTML = data.wind.speed + "mph"; 
 
-if(data.weather[0].main =="Clouds"){
-    weatherIcon.src = "images/clouds.png";
-}
-else if(data.weather[0].main == "Clear"){
-    weatherIcon.src = "images/clear.png";
-}
-else if(data.weather[0].main == "Rain"){
-    weatherIcon.src = "images/rain.png";
-}
-else if(data.weather[0].main == "Drizzle"){
-    weatherIcon.src = "images/drizzle.png";
-}
-else if(data.weather[0].main == "Mist"){
-    weatherIcon.src = "images/mist.png";
-}
+        if(data.weather[0].main =="Clouds"){
+            weatherIcon.src = "images/clouds.png";
+        }
+        else if(data.weather[0].main == "Clear"){
+            weatherIcon.src = "images/clear.png";
+        }
+        else if(data.weather[0].main == "Rain"){
+            weatherIcon.src = "images/rain.png";
+        }
+        else if(data.weather[0].main == "Drizzle"){
+            weatherIcon.src = "images/drizzle.png";
+        }
+        else if(data.weather[0].main == "Mist"){
+            weatherIcon.src = "images/mist.png";
+        }
 
-document.querySelector(".weather").style.display = "block";
-document.querySelector(".error").style.display = "none";
+        document.querySelector(".weather").style.display = "block";
+        document.querySelector(".error").style.display = "none";
 
+        showForecast(city);
     }
-    
-    
-
 }
 
-searchButton.addEventListener("click", ()=>{
-    checkingWeather(searchBox.value);
+searchButton.addEventListener("click", () => {
+    const city = searchBox.value;
+    checkingWeather(city);
+});
 
-})
+searchBox.addEventListener("keypress", (e) => {
+    if (e.key === "Enter") {
+        const city = searchBox.value;
+        checkingWeather(city);
+    }
+});
+
+async function showForecast(city){
+    const response = await fetch(forecastUrl + city + `&appid=${apiKey}`);
+    if(response.status == 404) return;
+
+    const data = await response.json();
+    const forecastContainer = document.querySelector(".forecast-cards");
+    forecastContainer.innerHTML = "";
+
+    // Track days we already added
+    const addedDays = new Set();
+
+    data.list.forEach(item => {
+        const date = new Date(item.dt_txt);
+        const day = date.toLocaleDateString('en-US', { weekday: 'short' });
+
+        if (!addedDays.has(day)) {
+            addedDays.add(day);
+
+            const card = document.createElement("div");
+            card.className = "forecast-card";
+
+            let iconUrl = "";
+            switch(item.weather[0].main){
+                case "Clouds": iconUrl = "images/clouds.png"; break;
+                case "Clear": iconUrl = "images/clear.png"; break;
+                case "Rain": iconUrl = "images/rain.png"; break;
+                case "Drizzle": iconUrl = "images/drizzle.png"; break;
+                case "Mist": iconUrl = "images/mist.png"; break;
+                default: iconUrl = "images/clear.png";
+            }
+
+            card.innerHTML = `
+                <p>${day}</p>
+                <img src="${iconUrl}">
+                <p>${Math.round(item.main.temp)}°F</p>
+            `;
+
+            forecastContainer.appendChild(card);
+        }
+    });
+
+    document.querySelector(".forecast").style.display = "block";
+}
